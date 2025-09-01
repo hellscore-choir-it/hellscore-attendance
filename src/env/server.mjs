94393e167e8792fc95fecd6749855ed0c8f7eb41
Moshe Gordon Radian <3040818+VehpuS@users.noreply.github.com>
2025-09-01 03:01:17 +0300

    fix: remove crashes on missing env

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -13,14 +13,11 @@ if (!_serverEnv.success) {
     "❌ Invalid environment variables:\n",
     ...formatErrors(_serverEnv.error.format()),
   );
-  throw new Error("Invalid environment variables");
 }
 
-for (let key of Object.keys(_serverEnv.data)) {
+for (let key of Object.keys(_serverEnv.data || {})) {
   if (key.startsWith("NEXT_PUBLIC_")) {
     console.warn("❌ You are exposing a server-side env-variable:", key);
-
-    throw new Error("You are exposing a server-side env-variable");
   }
 }
 
